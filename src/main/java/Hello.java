31793d648b1725b7f933503a26fff8964d149754
Rotopu16 <rotopu@gmail.com>
2021-12-20 15:55:46 +0300

    Update added more hello feature

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,6 +2,8 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Developing Hello Feature");
+        System.out.println("Local Repo is already published to GitHup");
+        System.out.println("doing More Work");
 
     }
 }
